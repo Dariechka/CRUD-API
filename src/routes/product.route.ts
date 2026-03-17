7b4eff54f23f1de21685ccb285fa0b30504baffe
daryakolenchenko <daria.kolenchecko@gmail.com>
2026-03-17 15:49:11 +0100

    feat: create post product endpoint

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,32 +1,88 @@
 import type { FastifyInstance } from 'fastify'
 import { productService } from '../services/product.service'
 import { validate } from 'uuid'
+import type { Product } from '../types/interfaces'
+import { randomUUID } from 'node:crypto'
 
 export async function productRoutes(fastify: FastifyInstance) {
   fastify.get('/api/products', async (_, reply) => {
     return reply
       .code(200)
-      .send(productService.getProducts());
-  });
+      .send(productService.getProducts())
+  })
   fastify.get<{ Params: { id: string } }>('/api/products/:id', async (request, reply) => {
-    const { id } = request.params;
+    const {id} = request.params
 
     if (!validate(id)) {
       return reply
         .code(400)
-        .send({ message: 'Invalid productId format (must be UUID)' });
+        .send({message: 'Invalid productId format (must be UUID)'})
     }
 
-    const product = productService.getProductById(id);
+    const product = productService.getProductById(id)
 
     if (!product) {
       return reply
         .code(404)
-        .send({ message: `Product with id ${id} not found` });
+        .send({message: `Product with id ${id} not found`})
     }
 
     return reply
       .code(200)
-      .send(product);
-  });
+      .send(product)
+  })
+
+
+  fastify.register(async function (instance: FastifyInstance) {
+    instance.post<{
+      Body: Omit<Product, 'id'>;
+      Reply: Product;
+    }>('/api/products', {
+        schema: {
+          body: {
+            type: 'object',
+            required: ['name', 'description', 'price', 'category', 'inStock'],
+            properties: {
+              name: {type: 'string', minLength: 1},
+              description: {type: 'string', minLength: 1},
+              price: {type: 'number', exclusiveMinimum: 0},
+              category: {type: 'string', minLength: 1},
+              inStock: {type: 'boolean'},
+            },
+          },
+          response: {
+            400: {
+              type: 'object',
+              properties: {
+                message: {type: 'string'},
+              },
+            },
+          },
+        },
+      },
+      async (request, reply) => {
+        const newProduct = {
+          ...request.body,
+          id: randomUUID(),
+        }
+
+        const created = productService.postProduct(newProduct)
+        return reply
+          .code(201)
+          .send(created)
+      })
+    instance.setErrorHandler((error, request, reply) => {
+      if (error.validation) {
+        return reply.status(400).send({
+          message: 'Invalid product data',
+        })
+      }
+
+      request.log.error(error)
+
+      return reply.status(500).send({
+        message: 'Product route error',
+      })
+    })
+  })
 }
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,6 +1,11 @@
 import { products } from '../data/products'
+import type { Product } from '../types/interfaces'
 
 export const productService = {
   getProducts: () => products,
   getProductById: (id: string) => products.find(product => product.id === id),
+  postProduct: (product: Product) => {
+    products.push(product)
+    return product
+  },
 };
